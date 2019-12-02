347558dd0b8165517fdbdeb919ebda661df7c73a
Liam Ball <liamballin@gmail.com>
2019-12-03 01:24:57 +1300

    Add images and genre grouping, disable multiple explode

--- index.js
+++ index.js
@@ -7,18 +7,25 @@ var client_id = "15db5a5f73854fbaa5d4da1d7cb15123"
 var client_secret = "66ff80f04fab4d45800f5e304466e8ff"
 
 var spotifyApi = new SpotifyWebApi({
-    clientId:client_id,
-    clientSecret:client_secret
+    clientId: "15db5a5f73854fbaa5d4da1d7cb15123",
+    clientSecret:"66ff80f04fab4d45800f5e304466e8ff"
     // redirectUri:"localhost/auth"
 })
-spotifyApi.setAccessToken('BQBxN7Vl4hv_mwdxdMqRAwpv1OwMd_E_zY3r3mf92i8wgJRynVYolmLwwD-DwxDL9ZRwUDt0bIowtLbeDm4c4njfaMqBFFRs0iPja-UdOHDnX51SoqhxQmoAJJuqOWIFsRF7-O1Be2GjxA')
+spotifyApi.setAccessToken('BQBzkCU1IPgwMkXNP2uk9nIivBi-nu9u2TIUYJUL5Mt0hEEZXfKrx_ymou8sFynOdZs4HRL3ElyPlRC0Yq_Mn7BXSA3U8DqY4jIeYB6T8v1b-ZQZQy9kUjk-4bCoLJDr4kWadARlJ3et_A')
 
 
 app.get("/getArtist/:id", (req,res)=>{
     getArtist(req.params.id).then(data=>{
         res.send(data)
+    }).catch(error=>{
+        console.log(error)
+        res.send("EROOR HOMIE")
     })
     
+})
+
+app.get("/new", (req,res)=>{
+    res.sendFile(__dirname+"/web/nodes.html")
 })
 
 app.get("/", (req,res)=>{
