04ff59ef00b1dde884cdcb9124055e0f8cf2b70a
Liam Ball <liamballin@gmail.com>
2019-12-03 03:11:19 +1300

    Add artist search

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var spotifyApi = new SpotifyWebApi({
     clientSecret:"66ff80f04fab4d45800f5e304466e8ff"
     // redirectUri:"localhost/auth"
 })
-spotifyApi.setAccessToken('BQB9xnmj8J4D5Oz1ZtthlU5oMYYqvnCGe-a_dh18PnmD0CBdnT_W1xxaW7egbsRFjwAM1fFjGUUR-o_VXBDScT2usbpq6-xoFbP83CN82X1Vf6qWpl4A3-va57HTxdP00mq4QZ7tWERvQg')
+spotifyApi.setAccessToken('BQCktlDDAcfJVBdOOI6w44tlRnKMMMmhOtCgfjAQkWpDndpkMitL1K9nBIVE4AwwbiwU0GB8olrhKSC3KAj1JZ5QhmR1VhZF_qDkz3DWGBlsGg0z_FBqRlkXERzlttHVQy27CUNSEQCGkQ')
 
 
 app.get("/getArtist/:id", (req,res)=>{
@@ -32,6 +32,15 @@ app.get("/preview/:id", (req,res)=>{
     })
 })
 
+app.get("/search/:query", (req,res)=>{
+    searchResults(req.params.query).then(data=>{
+        res.send(data);
+    }).catch(error=>{
+        console.log(error);
+        res.send("ERROR GETTING SEARCH RESULTS")
+    })
+})
+
 app.get("/new", (req,res)=>{
     res.sendFile(__dirname+"/web/nodes.html")
 })
@@ -43,6 +52,16 @@ app.get("/", (req,res)=>{
 app.listen(port, ()=>{
     console.log("Listening on "+port)
 })
+
+function searchResults(query){
+    return new Promise((resolve, reject)=>{
+        spotifyApi.searchArtists(query).then(function(data) {
+            resolve(data.body);
+            }, function(err) {
+            reject(err);
+        });
+    })
+}
 
 function getPreview(id){
     return new Promise((resolve,reject)=>{
@@ -66,6 +85,5 @@ function getArtist(id){
         }
       );
     })
-    
 }
 
