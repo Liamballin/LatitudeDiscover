805a77ffc5cb61adace7a3279d01a435996b1406
Liam Ball <liamballin@gmail.com>
2019-12-05 16:09:47 +1300

    Add .env

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 // var secrets;
+const dotenv = require('dotenv');
+dotenv.config();
 var SpotifyWebApi = require('spotify-web-api-node');
 
 // var envi;
